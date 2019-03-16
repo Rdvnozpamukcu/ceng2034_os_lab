72e487317cc3145e5dea73dbf752f8e666ec3b53
Rdvnozpamukcu <rozpamukcu1@gmail.com>
2019-03-16 19:33:00 +0300

    Start with sudo command.
    Maybe you need to use sudo command for open this script

---
+++ chjail.py
@@ -0,0 +1,23 @@
+import os
+import math
+
+print(os.getcwd())
+home = os.getenv("HOME")
+os.chdir(home)                                          #change directory to user's home
+
+
+os.mkdir('jail')                                        #creating jail directory.
+os.chdir('jail')                                        #change directory to jail
+os.mkdir('bin')                                         #create bin directory
+os.mkdir ('etc')                                        #create etc directory
+os.mkdir ('lib')                                        #create lib directory
+os.mkdir('lib64')                                       #create lib64 directory
+
+os.system("cp -ru $(which bash) ~/jail/bin/")	        #copying the dependencies of bash
+os.system("cp -ru /lib/x86_64-linux-gnu  ~/jail/lib/ ")
+os.system("cp -ru /lib64/ ~/jail/")
+
+os.chroot(home + "/jail")
+print(os.getcwd())
+print(math.sqrt(16))
+print(os.getcwd())
